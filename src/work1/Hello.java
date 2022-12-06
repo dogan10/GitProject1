aa43a577bf5898162d876aa1bf01a8c67b529b38
Dogan Tonbul <dogantonbull@gmail.com>
2022-12-06 10:42:41 +0100

    not first commit

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
         System.out.println("Work1 started");
         System.out.println("Created Git Repository");
         System.out.println("Made few commits");
+        System.out.println("first commit");
+
 
     }
 }
